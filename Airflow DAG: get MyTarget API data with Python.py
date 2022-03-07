aa6e16d31d1140958e4b09d7313fec820fcf4c54
AnastasiiaFomich <nastya4mina@gmail.com>
2022-03-07 20:02:04 +0300

    Update Airflow DAG: get MyTarget API data with Python.py

--- Airflow DAG: get MyTarget API data with Python.py
+++ Airflow DAG: get MyTarget API data with Python.py
@@ -92,32 +92,21 @@ def get_mt_banners(**context):
       #      offset = offset + 50
        #     try:
         #        if len(banners.json().get('items')) == 0:
-         #           cont=False
-          #      df_banners = pd.concat([df_banners, json_normalize(banners.json(), 'items')])
-           # except:
             #    continue
-    # получаем список всех баннеров
-    #df_banners = pd.DataFrame()
 
     def func2(tokens):
         df_banners = pd.DataFrame()
-        #for token in tokens:    
 
-            #offset=0
         def func(token_, offset,cont_,df_banners2):
             df_banners3 = df_banners2
             offset_2=offset
             cont2 = cont_
-            #pdb.set_trace()
-            #def func3(offset):
+
             while cont2 :
                 banners = requests.get('https://target.my.com/api/v2/banners.json?limit=50&offset={}'.format(offset_2), headers={'Authorization': 'Bearer {}'.format(token_)})
 
-               # print( '_', banners.headers, '_', 'status_code',banners.status_code)
                 print( '_',  '_', 'status_code',banners.status_code)
                 if banners.status_code == 200:
-                    print('yeeeeaahhh')
-                    #time.sleep(0.3)
                     offset_2 = offset_2 + 50
 
                     if banners.json().get('items') is None or len(banners.json().get('items')) == 0:
@@ -135,40 +124,30 @@ def get_mt_banners(**context):
 
                     time.sleep(1)
                     df_banners3 = func(token, offset_2,cont2,df_banners3)
-                    #df_banners3=df_banners3.append(b)
             return df_banners3
         for token in tokens:
             h = func(token, 0, True,pd.DataFrame())
             df_banners = df_banners.append(h)
         return df_banners
 
-                #df_banners = pd.concat([df_banners, json_normalize(banners.json(), 'items')])
-            #except:
-             #   continue
     df_banners = func2(tokens)
     df_banners = df_banners.drop_duplicates()
-    # получаем список всех кампаний
-    #df_banners = pd.DataFrame()
 
     def func2_c(tokens):
         df_banners = pd.DataFrame()
-        #for token in tokens:    
 
-            #offset=0
         def func_c(token_, offset,cont_,df_banners2):
             df_banners3 = df_banners2
             offset_2=offset
             cont2 = cont_
-            #pdb.set_trace()
-            #def func3(offset):
+
             while cont2 :
                 banners = requests.get('https://target.my.com/api/v2/campaigns.json?limit=50&offset={}'.format(offset_2), headers={'Authorization': 'Bearer {}'.format(token_)})
 
-               # print( '_', banners.headers, '_', 'status_code',banners.status_code)
+
                 print( '_',  '_', 'status_code',banners.status_code)
                 if banners.status_code == 200:
-                    print('yeeeeaahhh')
-                    #time.sleep(0.3)
+
                     offset_2 = offset_2 + 50
 
                     if banners.json().get('items') is None or len(banners.json().get('items')) == 0:
@@ -186,64 +165,33 @@ def get_mt_banners(**context):
 
                     time.sleep(1)
                     df_banners3 = func(token, offset_2,cont2,df_banners3)
-                    #df_banners3=df_banners3.append(b)
+
             return df_banners3
         for token in tokens:
             h = func_c(token, 0, True,pd.DataFrame())
             df_banners = df_banners.append(h)
         return df_banners
 
-                #df_banners = pd.concat([df_banners, json_normalize(banners.json(), 'items')])
-            #except:
-             #   continue
-
-    # выгрузка всех кампаний
     df_campaigns = func2_c(tokens)
     df_campaigns = df_campaigns.drop_duplicates()
     print('shapig', df_banners.shape, df_campaigns.shape )
-#    for token in tokens:
- #       cont = True
-  #      offset=0
-   #     while cont:
-    #        response = requests.get('https://target.my.com/api/v2/campaigns.json?limit=50&offset={}'.format(offset), headers={'Authorization': 'Bearer {}'.format(token)})
-     #       offset = offset + 50
-      #      try:
-       #         if len(banners.json().get('items')) == 0:
-        #            cont=False
-         #       df_campaigns = pd.concat([df_campaigns, json_normalize(response.json(), 'items')])
-          #  except:
-           #     continue
-        # объединяем баннеры и кампании
+
     df_campaigns.columns=['campaign_id', 'name', 'package_id']
     df_banners = df_banners.merge(df_campaigns, on='campaign_id', how='left')[['name', 'campaign_id', 'id']]
     df_banners.columns = ['campaign_name', 'campaign_id', 'id']
 
     date1 = (datetime.today() - timedelta(days=fom)).strftime("%Y-%m-%d")
     date2 = (datetime.today() - timedelta(days=to)).strftime("%Y-%m-%d")
-        # получаем список всех баннеров
-    #df_banners = pd.DataFrame()
 
-    # получаем список всех баннеров
-    #df_banners = pd.DataFrame()
 
     def func2_users(tokens, date_from, date_to):
 
         report = pd.DataFrame()
-        #for token in tokens:    
 
-            #offset=0
         def func_users(token_, from_, to_, df_stats):
             report = df_stats
-            #offset_2=offset
-            #cont2 = cont_
-            #pdb.set_trace()
-            #def func3(offset):
-
-#            for day in range(fom,to,-1):
-#            print(day)
-#            date = (datetime.today() - timedelta(days=day)).strftime("%Y-%m-%d")
             response = requests.get('https://target.my.com/api/v2/statistics/users/day.json?date_from={}&date_to={}&metrics=base'.format(from_,to_), headers={'Authorization': 'Bearer {}'.format(token_)})
-                #try:
+
             print(response.headers, ' status_code ', response.status_code)
             if response.status_code == 200:
                 print('yeeeeaahhh')
@@ -256,27 +204,7 @@ def get_mt_banners(**context):
             elif response.status_code == 429:
                 time.sleep(1)
                 report = func_users(token_, from_, to_, report)
-                        #df_banners3=df_banners3.append(b)
             return report
-    #         date1 = (datetime.today() - timedelta(days=fom)).strftime("%Y-%m-%d")
-    #         date2 = (datetime.today() - timedelta(days=to)).strftime("%Y-%m-%d")
-    #         response = requests.get('https://target.my.com/api/v2/statistics/banners/day.json?date_from={}&date_to={}&metrics=base'.format(date1,date2), headers={'Authorization': 'Bearer {}'.format(token_)})
-    #         #try:
-    #         print(response.headers, ' status_code ', response.status_code)
-    #         if response.status_code == 200:
-    #             print('yeeeeaahhh')
-    #             if response.json().get('items') is not None or len(response.json().get('items')) != 0  :
-
-    #                 df = json_normalize(response.json(), 'items').join(json_normalize(response.json(), ['items','rows']))
-    #                 df['token'] = token
-    #                 report = report.append(df)
-    #         elif response.status_code == 429:
-
-    #                 time.sleep(0.5)
-    #                 report = func_campaigns(token_, fom, to, report)
-    #                 #df_banners3=df_banners3.append(b)
-    #         return report
-
 
         for token in tokens:
             print(token)
@@ -286,25 +214,19 @@ def get_mt_banners(**context):
 
     users = func2_users(tokens, date1, date2)
     users_not_empty = set(users['token'][users['total.base.shows'] != 0])
-    print('UNEEEEEE',users_not_empty)
+    print('users_not_empty', users_not_empty)
     def func2_campaigns(tokens, date_from, date_to):
 
         report = pd.DataFrame()
-        #for token in tokens:    
 
-            #offset=0
         def func_campaigns(token_, fom, to, df_stats):
             report = df_stats
-            #offset_2=offset
-            #cont2 = cont_
-            #pdb.set_trace()
-            #def func3(offset):
 
             for day in range(fom,to,-1):
                 print(day)
                 date = (datetime.today() - timedelta(days=day)).strftime("%Y-%m-%d")
                 response = requests.get('https://target.my.com/api/v2/statistics/banners/day.json?date_from={}&date_to={}&metrics=base'.format(date,date), headers={'Authorization': 'Bearer {}'.format(token_)})
-                #try:
+
                 print(response.headers, ' status_code ', response.status_code)
                 if response.status_code == 200:
                     print('yeeeeaahhh')
@@ -318,28 +240,8 @@ def get_mt_banners(**context):
 
                     time.sleep(1)
                     report = func_campaigns(token_, fom, to, report)
-                      #df_banners3=df_banners3.append(b)
+
             return report
-
-    #         date1 = (datetime.today() - timedelta(days=fom)).strftime("%Y-%m-%d")
-    #         date2 = (datetime.today() - timedelta(days=to)).strftime("%Y-%m-%d")
-    #         response = requests.get('https://target.my.com/api/v2/statistics/banners/day.json?date_from={}&date_to={}&metrics=base'.format(date1,date2), headers={'Authorization': 'Bearer {}'.format(token_)})
-    #         #try:
-    #         print(response.headers, ' status_code ', response.status_code)
-    #         if response.status_code == 200:
-    #             print('yeeeeaahhh')
-    #             if response.json().get('items') is not None or len(response.json().get('items')) != 0  :
-
-    #                 df = json_normalize(response.json(), 'items').join(json_normalize(response.json(), ['items','rows']))
-    #                 df['token'] = token
-    #                 report = report.append(df)
-    #         elif response.status_code == 429:
-
-    #                 time.sleep(0.5)
-    #                 report = func_campaigns(token_, fom, to, report)
-    #                 #df_banners3=df_banners3.append(b)
-    #         return report
-
 
         for token in tokens:
             print(token)
@@ -347,19 +249,6 @@ def get_mt_banners(**context):
             report = report.append(h)
         return report
     report = func2_campaigns(users_not_empty, fom, to)
-    # делаем выгрузку за несколько дней
- #   report = pd.DataFrame()
-  #  for token in tokens:
-   #     for day in range(fom,to,-1):
-    #        date = (datetime.today() - timedelta(days=day)).strftime("%Y-%m-%d")
-     #       response = requests.get('https://target.my.com/api/v2/statistics/banners/day.json?date_from={}&date_to={}&metrics=base'.format(date,date), headers={'Authorization': 'Bearer {}'.format(token)})
-      #      try:
-       #         df = json_normalize(response.json(), 'items').join(json_normalize(response.json(), ['items','rows']))
-        #        df['token'] = token
-         #       report = report.append(df)
-          #  except:
-           #     continue
-        # добавляем в выгрузку имя клиента и название кампаний
     cols = [i for i in report.columns if "total" not in i]
     report = report[cols].drop('rows', axis=1)
     MT_repor = report.reset_index(drop=True) 
@@ -382,72 +271,21 @@ def get_mt_banners(**context):
     MT_repor['Business_date'] = datetime.now().replace(microsecond=0)
     MT_repor['Business_date'] =  pd.to_datetime(MT_repor['Business_date'], errors='coerce')
     MT_repor['Platform']='MT'
-    MT_repor['Sessions']=None
-    MT_repor['Bounces']=None
-    MT_repor['Reach']=None
-    MT_repor['AdGroup_Id']=None
-    MT_repor['AdGroup_Name']=None
-    MT_repor['Ad_name']=None
     MT_repor.drop(['base.goals','base.cpm','base.cpc', 'base.cpa', 'base.ctr', 'base.cr'],axis=1,inplace=True)
-    MT_repor['index'] = None
-    MT_repor['medium'] = None
-    MT_repor['Business_date_GA'] = None
-    MT_repor['keyword'] = None
-    MT_repor['campaign_GA'] = None
-    MT_repor['date_GA'] = None
-    MT_repor['sourceMedium'] = None
-    MT_repor['adContent'] = None
-    MT_repor['source'] = None
-    MT_repor['goal1Completions'] = None
-    MT_repor['sessions_GA'] = None
-    MT_repor['goal2Completions'] = None
-    MT_repor['impressions_GA'] = None
-    MT_repor['sessionDuration_GA'] = None
-    MT_repor['click_GA'] = None
-    MT_repor['goal3Completions'] = None
-    MT_repor['adCost'] = None
-    MT_repor['bounces_GA'] = None
-    MT_repor['adContent - for YA'] = None
-    MT_repor['adContent - for FB'] = None
-   # MT_repor['Ad_id'] = MT_repor['Ad_id'].fillna(0)
     MT_repor['Ad_id']= MT_repor['Ad_id'].astype(int)
-   # MT_repor['Ad_id']= MT_repor['Ad_id'].astype(str)
-   # MT_repor['Ad_id'][MT_repor['Ad_id'] == '0']= None
-   # MT_repor['Campaign_id'] = MT_repor['Campaign_id'].fillna(0)
     MT_repor['Campaign_id']= MT_repor['Campaign_id'].astype(int)
-  #  MT_repor['Campaign_id']= MT_repor['Campaign_id'].astype(str)
-   # MT_repor['Campaign_id'][MT_repor['Campaign_id'] == '0']= None
-    MT_repor['1'] = None
-    MT_repor['2'] = None
-    MT_repor['3'] = None
-    MT_repor['4'] = None
-    MT_repor['5'] = None
-    MT_repor['6'] = None
-    MT_repor['7'] = None
-    MT_repor['8'] = None
-    MT_repor['9'] = None
-    MT_repor['10'] = None
-    MT_repor['11'] = None
-    MT_repor['12'] = None
-    MT_repor['13'] = None
-    MT_repor['14'] = None
-    MT_repor['15'] = None
-    MT_repor['16'] = None
-    MT_repor['17'] = None
-    MT_repor['18'] = None
-    MT_repor['19'] = None
-    MT_repor['20'] = None
     MT_repor = MT_repor.drop_duplicates()
     context['ti'].xcom_push(key = 'clients_df_mt', value = MT_repor) 
     return MT_repor
 
-engine ='postgresql+psycopg2://airflow@localhost:8123/digitaladsdb'
+
+
+engine =''
+
+# declare Python function for DAG Task 3 (upload data to local PostgreSQL Database):
 def upload_mt_data_to_db(**context):
-#    postgres_hook = PostgresHook(self.postgres_conn_id)
     global engine
-#engine = postgres_hook.get_sqlalchemy_engine()
     table = context['ti'].xcom_pull(key = 'clients_df_mt') 
-   # engine = create_engine('postgresql://airflow:y59RaFJKqy@192.168.127.16:8123/digitalads')
     table.to_sql('mt_pre2', engine, if_exists='append', index = False)
     
 with DAG(**dag_params) as dag:
@@ -458,8 +296,7 @@ with DAG(**dag_params) as dag:
         python_callable=get_mt_clients,
         dag=dag,
     )
-#    print(type(create_table))    
-#with DAG(**dag_params) as dag:
+
     create_table2_mt = PythonOperator(
         task_id='download_data2_mt',
         python_callable=get_mt_banners,
@@ -469,8 +306,6 @@ with DAG(**dag_params) as dag:
     drop_table_mt = PostgresOperator(
         sql='delete from mt_pre2;''',
         task_id='drop_table_mt'
-    #    provide_context = True,
-   #     python_callable=upload_data_to_db,
         )
     insert_table_mt = PythonOperator(
         task_id='append_table_mt',
@@ -478,19 +313,7 @@ with DAG(**dag_params) as dag:
         python_callable=upload_mt_data_to_db,
         dag=dag,
     )
-#    join_table_mt = PostgresOperator(
- #       task_id='join_table_mt',
-  #      sql='''insert into all_channels_ga ("Ad_id", "Date", "Campaign_id", "Campaign_name",  "Impressions", "Clicks",  "Platform", "Spend", "Client_name", "Client_id")
-#select "Ad_id", "Date", "Campaign_id", "Campaign_name",  "Impressions", "Clicks",  "Platform", "Spend", "Client_name", "Client_id" from mt_pre2
-#where "Ad_id"  not in (select distinct "Ad_id" from all_channels_ga where "Platform" = 'MT') 
-#or "Date"  not in (select distinct "Date" from all_channels_ga where "Platform" = 'MT');''',
- #        dag=dag, )
-  #  update_table_mt = PostgresOperator(
-   #     task_id='update_table_mt',
-    #    sql='''UPDATE all_channels_ga SET "Impressions" = mt_pre2."Impressions", "Business_date" = mt_pre2."Business_date", "Clicks" = mt_pre2."Clicks", "Bounces" = mt_pre2."Bounces", "Spend" = mt_pre2."Spend", "Sessions" = mt_pre2."Sessions",
-# "Reach" =  mt_pre2."Reach"
- # FROM   mt_pre2 where all_channels_ga."Ad_id" = mt_pre2."Ad_id" and all_channels_ga."Date" = mt_pre2."Date"    ;''',
-  #      dag=dag, )
+
     join_table_mt = PostgresOperator(
         task_id='join_table_mt',
         sql='''insert into all_channels_ga select mt_pre2.* from mt_pre2 left join all_channels_ga on all_channels_ga."Ad_id" = mt_pre2."Ad_id" and all_channels_ga."Date" = mt_pre2."Date"  
@@ -507,13 +330,11 @@ where all_channels_ga."Ad_id" is null or  all_channels_ga."Date" is null;''',
 
     delete_xcom_task_mt = PostgresOperator(
         task_id='delete-xcom-task_mt',
-#      postgres_conn_id='airflow_db',
         sql='''delete from xcom where dag_id=dag_id and
            task_id='download_data_mt' ''',
         dag=dag)
     delete_xcom_task2_mt = PostgresOperator(
         task_id='delete-xcom-task2_mt',
-#      postgres_conn_id='airflow_db',
         sql='''delete from xcom where dag_id=dag_id and
            task_id='download_data2_mt' ''',
         dag=dag)
